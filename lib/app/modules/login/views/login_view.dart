0e1b9ac005ef4b5a62faf16b276b0173bca35638
Adelia Oktaviani <adeliaaa.ok@gmail.com>
2022-07-07 15:48:27 +0700

    first commit

---
+++ lib/app/modules/login/views/login_view.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+
+import '../controllers/login_controller.dart';
+
+class LoginView extends GetView<LoginController> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Container(
+        margin: EdgeInsets.all(Get.height * 0.1),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(50),
+          color: Colors.white,
+        ),
+        child: Row(children: [
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50),
+                  bottomLeft: Radius.circular(50),
+                ),
+                color: Colors.blue,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        'Welcome',
+                        style: TextStyle(color: Colors.white, fontSize: 70),
+                      ),
+                      Text(
+                        'Please Sign In',
+                        style: TextStyle(color: Colors.white, fontSize: 30),
+                      ),
+                      Text(
+                        'Start Journey With Us',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                    ]),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50),
+                  bottomRight: Radius.circular(50),
+                ),
+              ),
+              child: Column(children: [
+                Image.asset('assets/images/login.png'),
+                FloatingActionButton.extended(
+                  onPressed: () {},
+                  label: const Text("Sign in with google"),
+                  icon: const Icon(
+                    Icons.android,
+                    color: Colors.white,
+                  ),
+                ),
+              ]),
+            ),
+          )
+        ]),
+      ),
+    );
+  }
+}
+
+class Ionicons {}
---
+++ lib/app/utils/widget/header.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../../modules/login/views/login_view.dart';
+import '../../routes/app_pages.dart';
+import '../style/AppColors.dart';
+
+// ignore: camel_case_types
+class header extends StatelessWidget {
+  const header({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: Get.height * 0.1,
+      child: Padding(
+        padding: const EdgeInsets.only(left: 40, right: 40, top: 20),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              //mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Text(
+                  'Task Management',
+                  style: TextStyle(fontSize: 20, color: AppColors.primaryText),
+                ),
+                const Text(
+                  'manage task made easy with firend',
+                  style: TextStyle(fontSize: 14, color: AppColors.primaryText),
+                )
+              ],
+            ),
+            const Spacer(
+              flex: 1,
+            ),
+            Expanded(
+              flex: 1,
+              child: TextField(
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  contentPadding: const EdgeInsets.only(left: 40, right: 10),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.white),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: const BorderSide(color: Colors.blue),
+                  ),
+                  // ignore: prefer_const_constructors
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: AppColors.primaryText,
+                  ),
+                  hintText: 'Search',
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            const Icon(
+              Icons.notifications,
+              color: AppColors.primaryText,
+            ),
+            const SizedBox(
+              width: 20,
+            ),
+            GestureDetector(
+              onTap: () {
+                Get.defaultDialog(
+                  title: 'Sign Out',
+                  content: const Text('are you sure want to sign out?'),
+                  cancel: ElevatedButton(
+                    onPressed: () => Get.back(),
+                    child: const Text('Cancel'),
+                  ),
+                  confirm: ElevatedButton(
+                    onPressed: () => Get.toNamed(Routes.LOGIN),
+                    child: const Text('Sign Out'),
+                  ),
+                );
+              },
+              child: Row(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  const Text(
+                    'Sign Out',
+                    style: TextStyle(color: AppColors.primaryText),
+                  ),
+                  const SizedBox(
+                    width: 5,
+                  ),
+                  const Icon(Icons.logout, color: AppColors.primaryText)
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
